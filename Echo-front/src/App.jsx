136076541ecf1e03436fb26384d77f0691f7f60f
mohamedtaghian <mohamedtaghian12@gmail.com>
2026-04-13 19:56:51 +0200

    feat(App.jsx): added home page router

--- Echo-front/src/App.jsx
+++ Echo-front/src/App.jsx
@@ -3,13 +3,17 @@ import Authentication from "./pages/Authentication/Authentication";
 import Layout from "./pages/Layout/Layout";
 import AuthProvider from "./providers/AuthProvider/AuthProvider";
 import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home/Home";
 
 function App() {
   const routes = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
-      children: [{ path: "/auth", element: <Authentication /> }],
+      children: [
+        { path: "/auth", element: <Authentication /> },
+        { path: "/", element: <Home /> },
+      ],
     },
   ]);
 
