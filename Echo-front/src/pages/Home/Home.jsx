73db453add652b0cb032cd1fe8ccfb60379a7e9a
mohamedtaghian <mohamedtaghian12@gmail.com>
2026-04-13 20:00:54 +0200

    feat(Home.jsx): added Home page

---
+++ Echo-front/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../../components/NavBar/NavBar";
+
+function Home() {
+  return (
+    <div className="bg-black/90 pt-2.5 min-h-screen">
+      <NavBar />
+    </div>
+  );
+}
+
+export default Home;
